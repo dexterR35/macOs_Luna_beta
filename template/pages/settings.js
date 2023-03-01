6ed78df47901a8741d1a8bd96d2fc47faf42e274
DexTerR33 <pusaw202@gmail.com>
2023-03-01 17:34:08 +0200

    update header

--- template/pages/settings.js
+++ template/pages/settings.js
@@ -31,9 +31,10 @@ export function openNavLink() {
       closeOpenItems();
     }
   });
+  appendStuff();
 };
 
-let rangeInput = document.getElementById("rangeInput");
+// let rangeInput = document.getElementById("rangeInput");
 // let output = document.getElementById("output");
 
 // output.innerHTML = rangeInput.value;
@@ -41,3 +42,39 @@ let rangeInput = document.getElementById("rangeInput");
 // rangeInput.oninput = function() {
 //   output.innerHTML = this.value;
 // }
+
+
+function appendStuff() {
+  $(".nav_checkBox").append(`<input type="checkbox" class="checkBox_nav" checked>`)
+}
+
+export function startTime() {
+  var today = new Date();
+  var hr = today.getHours();
+  var min = today.getMinutes();
+  var sec = today.getSeconds();
+  // ap = (hr < 12) ? "<span>AM</span>" : "<span>PM</span>";
+  hr = (hr == 0) ? 12 : hr;
+  hr = (hr > 12) ? hr - 12 : hr;
+  //Add a zero in front of numbers<10
+  hr = checkTime(hr);
+  min = checkTime(min);
+  document.getElementById("clock").innerHTML = hr + ":" + min ;
+  
+  // var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  var curWeekDay = days[today.getDay()];
+  // var curDay = today.getDate();
+  // var curMonth = months[today.getMonth()];
+  // var curYear = today.getFullYear();
+  var date = curWeekDay;
+  document.getElementById("date").innerHTML = date;
+  
+  var time = setTimeout(function(){ startTime() }, 500);
+}
+function checkTime(i) {
+  if (i < 10) {
+      i = "0" + i;
+  }
+  return i;
+}
